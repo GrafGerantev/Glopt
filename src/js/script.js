e62350e2a9060b59b40f613852d9ba08fe774d10
Graf <mr.gerantev@mail.ru>
2019-10-17 17:57:27 +0300

    fixed работает, сделал ПОДРОБНЕЕ в каталоге

--- src/js/script.js
+++ src/js/script.js
@@ -17,13 +17,16 @@ $(document).ready(function(){
             }
         ]
     });
-
+    //Smooth scroll and pageup
     $(window).scroll(function () {
-        if($(this).scrollTop() > 727) {
-            $('.header').addClass('header-fixed');
+        if($(this).scrollTop() > 727)  {
+            if($('header').hasClass('header-fixed')){}
+            else {
+                $('.header').fadeOut(0).addClass('header-fixed').fadeIn();
+            }
         }
         else {
-            $('.header').removeClass('header-fixed');
+            $('.header').removeClass('header-fixed', 300);
         }
         if ($(this).scrollTop() > 1400) {
             $('.pageup').fadeIn();
@@ -34,9 +37,42 @@ $(document).ready(function(){
     });
     $("a[href^='#']").click(function(){
         const _href = $(this).attr("href");
-        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        $("html, body").animate({
+            scrollTop: $(_href).offset().top+"px"});
         return false;
     });
     //Плавное появление в связке с animated.css
     /*new WOW().init();*/
+    $('.burger').on('click', function() {
+        $(this).toggleClass('burger_active');
+        $('.header__nav').toggleClass('header__nav_active');
+    });
+    $('.header__link').on('click', function() {
+        $('.header__nav').toggleClass('header__nav_active');
+        /*console.log(this);*/
+        $('.burger').toggleClass('burger_active');
+    });
+    
+/*    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__first').eq(i).toggleClass('catalog-item__first_active');
+                $('.catalog-item__second').eq(i).toggleClass('catalog-item__second_active');
+            });
+        });
+    };*/
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__first').eq(i).toggleClass('catalog-item__first_active');
+                $('.catalog-item__second').eq(i).toggleClass('catalog-item__second_active');
+                /*console.log(this);*/
+            });
+        });
+    };
+    toggleSlide('.catalog-button');
+    toggleSlide('.catalog-back');
+
 });
